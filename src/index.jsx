8c4f9d14193822fa91996e49c456152bf49f02be
Kateřina Levová <katerina.levova@gmail.com>
2021-05-22 10:56:02 +0200

    Router rozchozen

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,27 @@ import { render } from 'react-dom';
 import './style.css';
 import Header from './Header/index';
 import Footer from './Footer/index';
+import Home from './Home/index';
+import Reservation from './Reservation';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const App = () => (
-  <div className="container">
-    <Header />
-    <Footer />
-  </div>
-);
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Link to="/">
+        <Home />
+      </Link>
+      <Link to="/reservation">
+        <Reservation />
+      </Link>
+      <Switch>
+        <Route path="/" exact></Route>
+        <Route path="/reservation"></Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
