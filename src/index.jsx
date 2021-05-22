6436e330615567ab954ee8046218139f09475043
Kateřina Levová <katerina.levova@gmail.com>
2021-05-22 10:43:55 +0200

    Header a Footer vytvořeny

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './Header/index';
+import Footer from './Footer/index';
 
 const App = () => (
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Header />
+    <Footer />
   </div>
 );
 
